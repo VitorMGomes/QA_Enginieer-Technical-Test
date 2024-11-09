73e437f6d1d955bef7462a5ded5cdaa91ca94f0e
Vitor <vitormeiragomes@outlook.com>
2024-11-08 22:28:01 -0300

    Configurado para abrir o site no script do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://www.automationpractice.pl/index.php?controller=authentication&back=my-account',
+    testIsolation: false,
   },
 });
